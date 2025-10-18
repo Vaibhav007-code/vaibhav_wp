f12cdaf35c78ba733d95fdcec0314d656eea75f3
VAIBHAV PATHAK <129991855+Vaibhav007-code@users.noreply.github.com>
2025-10-18 12:06:06 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Chromium dependencies
+apt-get update
+apt-get install -y \
+    chromium \
+    chromium-driver \
+    fonts-liberation \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libcups2 \
+    libdbus-1-3 \
+    libdrm2 \
+    libgbm1 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    xdg-utils
+
+# Install backend dependencies
+cd backend
+npm install
